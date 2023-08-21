d675bc8bcb8113fa44550b316b92df5e0e30a5f7
papriabhuiyan <56096609+papriabhuiyan@users.noreply.github.com>
2023-08-20 23:02:51 -0500

    changed ui elements of the page, added bio

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -22,14 +22,14 @@ const About = () => {
         ></img>
       </div>
       <div className="about-box">
-        <p>
-          I am a software engineer who holds a Bachelor of Science degree in Computer Science from <span className="hl">
-            Southern Methodist University</span>. Currently I am engaged in the pursuit of a Master's degree in <span className='hl'>Operations Research and 
-            Engineering Management</span>, also from SMU.
-          Presently, I am also a <span className="hl"> Full-Stack Engineer at Cognizant</span>. My professional journey reflects my 
+        <p className='aboutMe'>
+          <span className="main-writing">I am a software engineer who holds a Bachelor of Science degree in Computer Science from </span><span className="hl">
+            Southern Methodist University</span><span className="main-writing">. Currently I am engaged in the pursuit of a Master's degree in </span><span className='hl'>Operations Research and 
+            Engineering Management</span><span className="main-writing">, also from SMU.
+          Presently, I am also a </span><span className="hl"> Full-Stack Engineer at Cognizant</span><span className="main-writing">. My professional journey reflects my 
           unwavering commitment to continuous learning and growth
-           within the technology sector. I am passionate about <span className='hl'>problem solving</span> which fuels my aspiration 
-           to <span className='hl'>tackle challenges</span> that demand innovative solutions.
+           within the technology sector. I am passionate about </span><span className='hl'>problem solving</span><span className="main-writing"> which fuels my aspiration 
+           to </span><span className='hl'>tackle challenges</span><span className="main-writing"> that demand innovative solutions.</span>
         </p>
       </div>
     </div>
