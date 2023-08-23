a8169a3208529f0ac2cdb735011989565207a0d0
papriabhuiyan <56096609+papriabhuiyan@users.noreply.github.com>
2023-08-23 04:42:39 -0500

    made more changes to the projects part of the UI

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -7,74 +7,108 @@ const Projects = () => {
   const projects = [
     {
       name: "Perform Here",
-      info: "A responsive web-based application helping students and young professionals alike find rooms and roommates around the San Francisco Bay Area.",
-      skill: "React  CSS  Express  MySQL  Firebase",
-      demo: "https://youtu.be/vsSw3u31dvY",
+      info: "A responsive web-based application that enables artists to gauge local concert interest by collecting deposites from individuals interested in potential concert events",
+      skill: "HTML  CSS  Angular  Java Spring Boot  MySQL  AWS  Stripe API",
+      demo: "",
       demo_type: "Youtube Demo",
       source: "https://github.com/papriabhuiyan/Perform-Here-App",
       img: "img-perform-here-landing",
     },
     {
       name: "Pixelgram",
-      info: "This is a photo management website that allows users to post images, search for images and view and comment on individual image posts.",
-      skill: "HTML  CSS  JavaScript  Handlebars  Express",
-      demo: "https://youtu.be/OyRgzGGD7uM",
+      info: " A responsive instagram clone, social media web app that allows users to interact with each other by following each other, posting pictures and viewing, liking, and commenting on posts",
+      skill: "HTML  CSS  Angular  Java Spring Boot  MySQL  AWS",
+      demo: "",
       demo_type: "Youtube Demo",
       source: "https://github.com/papriabhuiyan/Pixelgram",
-      img: "img-photo-app",
+      img: "img-pixelgram",
     },
     {
       name: "AI Shirt Customizer",
-      info: "This SF Hacks website I redesigned during SF Hacks 2021 and won a Web Competition for the best redesign.",
-      skill: "HTML  CSS  JavaScript  React",
-      demo: "https://burmy.github.io/sfhacks/?",
+      info: "A responsive web app that allows the user to customize their shirt by changing the color of the shirt, adding different logos and prints on the shirt by using DALL.E",
+      skill: "HTML  CSS  React  OpenAI API",
+      demo: "",
       demo_type: "Live Demo",
       source: "https://github.com/papriabhuiyan/3D-product",
-      img: "img-hacks",
+      img: "img-ai-shirt",
     },
     {
       name: "Housing Data Analysis",
-      info: "This website tracks the wildfires going around the world using a NASA API.",
-      skill: "HTML  CSS  JavaScript  React  NASA API",
-      demo: "https://burmy.github.io/Wildfire-Tracker/?",
+      info: "A program that creates a Linear Regression Model to predict the price per sqft for the a large scale housing dataset",
+      skill: "Python",
+      demo: "",
       demo_type: "Live Demo",
       source: "https://github.com/papriabhuiyan/LinearRegressionModel",
-      img: "img-wildfire",
+      img: "img-lin-reg-model",
     },
   ];
   return (
-    <div className="center projects" id="projects">
+    // <div className="center projects" id="projects">
+    //   <div className="project-box">
+    //     <Fade>
+    //       <h1 className="center mp ">My Projects</h1>
+    //     </Fade>
+
+    //     {projects.map((project) => (
+    //       <Fade>
+    //         <div className="project-cont">
+    //           <div className="center project-card">
+    //             <div className="center project-data">
+    //               <h1 className="name">{project.name}</h1>
+    //               <p className="info">{project.info}</p>
+    //               <ul className="project-skills">
+    //                 <li className="skill">{project.skill}</li>
+    //               </ul>
+    //               <div className="project-links">
+    //                 {/* <form action={project.demo} method="get" target="_blank">
+    //                   <button className="demo">{project.demo_type}</button>
+    //                 </form> */}
+    //                 <form action={project.source} method="get" target="_blank">
+    //                   <button className="demo">View Source</button>
+    //                 </form>
+    //               </div>
+    //             </div>
+    //             <div className="center project-image">
+    //               <div className={project.img}></div>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </Fade>
+    //     ))}
+    //   </div>
+    // </div>
+    
+    <div className="projects-section" id="projects">
       <div className="project-box">
         <Fade>
-          <h1 className="center mp ">My Projects</h1>
+          <h1 className="myProjects">My Projects</h1>
         </Fade>
-
-        {projects.map((project) => (
-          <Fade>
-            <div className="project-cont">
-              <div className="center project-card">
-                <div className="center project-data">
+      </div>
+      <div className="project-cont">
+        <div className="project-cards">
+          {projects.map((project)=> (
+              <Fade>
+                <div className="each-card">
+                <div className="center project-image">
+                  <div className={project.img}></div>
+                </div>
+                <div className="project-data">
                   <h1 className="name">{project.name}</h1>
                   <p className="info">{project.info}</p>
                   <ul className="project-skills">
                     <li className="skill">{project.skill}</li>
                   </ul>
                   <div className="project-links">
-                    <form action={project.demo} method="get" target="_blank">
-                      <button className="demo">{project.demo_type}</button>
-                    </form>
-                    <form action={project.source} method="get" target="_blank">
-                      <button className="demo">View Source</button>
-                    </form>
-                  </div>
+                 <form action={project.source} method="get" target="_blank">
+                   <button className="demo">View Source</button>
+                 </form>
+                 </div>      
                 </div>
-                <div className="center project-image">
-                  <div className={project.img}></div>
                 </div>
-              </div>
-            </div>
-          </Fade>
-        ))}
+              </Fade>
+          ))};
+        </div>
+
       </div>
     </div>
   );
